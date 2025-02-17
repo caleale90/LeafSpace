c8115f74a01c59e43dd7ded9324a5fff5c62606e
alessandro <caleale90@gmail.com>
2025-02-17 19:53:33 +0100

    * aggiunto testo per chiarire se non sono stati trovati cocktail o non è il momento della birra

--- test/suggestion/test_SuggestionResponse.py
+++ test/suggestion/test_SuggestionResponse.py
@@ -8,7 +8,7 @@ from lib.suggestion.SuggestionResponse import SuggestionResponse
 class TestSuggestionResponse(unittest.TestCase):
 
     def test_to_dict_when_no_cocktail_and_beer(self):
-        expected = {"beer": {}, "cocktail": {}}
+        expected = {'beer': {'no beer it\'s daytime'}, 'cocktail': {'no cocktail found'}}
 
         beer = None
         cocktail = None
@@ -22,12 +22,12 @@ class TestSuggestionResponse(unittest.TestCase):
                              'price': 8.4,
                              'rating_avg': 3.5,
                              'rating_reviews': 'reviews'},
-                    "cocktail": {"name": "name",
-                                 "tagline": "tagline",
-                                 "instructions": "ins",
-                                 "ingredients": "ingredients"}}
+                    'cocktail': {'name': 'name',
+                                 'tagline': 'tagline',
+                                 'instructions': 'ins',
+                                 'ingredients': 'ingredients'}}
 
-        beer = Beer("name", 8.4, 3.5, 'reviews', 'image')
+        beer = Beer('name', 8.4, 3.5, 'reviews', 'image')
         cocktail = Cocktail('name', 'tagline', 'ins', 'ingredients')
         result = SuggestionResponse(beer, cocktail).to_dict()
 
